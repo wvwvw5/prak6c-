5e2462c24fd800ffce953fa80d14c742b407e518
wvwvw5 <e.kononykhin@mail.ru>
2024-04-21 16:15:49 +0300

    1

---
+++ kalend/kalend/DayCard.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace kalend
+{
+    /// <summary>
+    /// Логика взаимодействия для DayCard.xaml
+    /// </summary>
+    public partial class DayCard : UserControl
+    {
+        public string Day { get; set; }
+
+        public DayCard()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void Image_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show(Day);
+        }
+    }
+
+
+
+
+}
+
---
+++ kalend/kalend/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace kalend
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    /// 
+
+
+    public partial class MainWindow : Window
+    {
+        DateTime day = DateTime.Now;
+        public MainWindow()
+        {
+            InitializeComponent();
+            plakiplaki();
+
+        }
+        
+        private void plakiplaki()
+        {
+            plaki.Text = day.ToString("MMMM, yyyy, г."); 
+            PeaceDanceDva.Children.Clear();
+            for (int i = 1; i <= DateTime.DaysInMonth(day.Year, day.Month); i++)
+            {
+                DayCard card = new DayCard();
+                card.Day = i.ToString(); 
+                PeaceDanceDva.Children.Add(card);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            day = day.AddMonths(1);
+            plakiplaki();
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            day = day.AddMonths(-1);
+            plakiplaki();
+        }
+    }
+}
+
